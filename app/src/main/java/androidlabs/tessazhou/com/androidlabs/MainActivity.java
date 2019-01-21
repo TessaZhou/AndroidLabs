0bac5abac77444e9ad2d8e1a95f4f88a8da875a5
heiya <zhou0151@algonquinlive.com>
2019-01-21 18:43:54 -0500

    Added 3 layouts for Lab2

--- app/src/main/java/androidlabs/tessazhou/com/androidlabs/MainActivity.java
+++ app/src/main/java/androidlabs/tessazhou/com/androidlabs/MainActivity.java
@@ -3,11 +3,11 @@ package androidlabs.tessazhou.com.androidlabs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Lab1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lab1);
+        setContentView(R.layout.activity_main_relative);
     }
 }
